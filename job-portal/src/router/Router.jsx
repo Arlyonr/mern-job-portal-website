c85fea2aae2b954130fcbc0f6b33c3e9f473eb9c
mdalmamunit427 <mdalmamunit427@gmail.com>
2023-11-07 10:27:16 +0600

    first commit

---
+++ job-portal/src/router/Router.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+import {createBrowserRouter,} from "react-router-dom";
+import App from '../App';
+import Home from '../pages/Home';
+import MyJobs from '../pages/MyJobs';
+import SalaryPage from '../pages/SalaryPage';
+import CreateJob from '../pages/CreateJob';
+import UpdateJob from '../pages/UpdateJob';
+import JobDetails from '../pages/JobDetails';
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      children: [
+        {
+            path: "/",
+            element: <Home/>
+        },
+        {
+            path: "/my-job",
+            element: <MyJobs/>
+        },
+        {
+            path: "/salary",
+            element: <SalaryPage/>
+        },
+        {
+          path: "/post-job",
+          element: <CreateJob/>
+        },
+        {
+          path: "edit-job/:id",
+          element: <UpdateJob/>,
+          loader: ({params}) => fetch(`http://localhost:5000/all-jobs/${params.id}`)
+        },
+        {
+          path:"/jobs/:id",
+          element: <JobDetails/>,
+        }
+      ]
+    },
+  ]);
+
+  export default router;
